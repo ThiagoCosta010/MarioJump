ff9a4ae1ade23bc13684da1bf7cdf8af38b2d855
Thiago Costa <tigurrock@gmail.com>
2022-11-25 10:22:51 -0300

    att

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
-const res = document.querySelector('.res')
-const btnReiniciar = document.querySelector('.btnReiniciar')
 
 const jump = () => {
     mario.classList.add('jump')
 
-    setTimeout(function() {
+    setTimeout(() => {
         mario.classList.remove('jump')
     }, 500)
 }
-const loop = setInterval( () => {
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
 
     console.log(loop)
 
-    const pipePosition = pipe.offsetLeft
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','')
-    if(pipePosition <= 80 && pipePosition > 0 && marioPosition <= 120){
-
+    if(pipePosition > 0 && pipePosition <= 120 && marioPosition < 80){
         pipe.style.animation = 'none'
         pipe.style.left = `${pipePosition}px`
 
         mario.style.animation = 'none'
         mario.style.bottom = `${marioPosition}px`
 
-        mario.setAttribute('src','img/game-over.png')
+        mario.setAttribute('src', 'img/game-over.png')
         mario.style.width = '75px'
         mario.style.marginLeft = '50px'
 
-        setInterval(() => {
+        setInterval(function() {
             location.reload()
-        },2000)
-        
+        }, 2000)
+
         setInterval(loop)
     }
 }, 10)
